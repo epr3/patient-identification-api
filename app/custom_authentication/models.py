67a40775b2d6cecb83bb4f327ea73d486a658fbe
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-11-12 21:15:50 +0200

    WIP user model

--- app/custom_authentication/models.py
+++ app/custom_authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+  email = models.EmailField(unique=True)
+  first_name = models.CharField()
+  last_name = models.CharField()
+  date_of_birth = models.DateField()
+  pnc = models.CharField(max_length=13)
+  insure_code = models.CharField()
+  expiry_date = models.DateField()
+
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
