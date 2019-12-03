9b92d9fe0e4921a2dbe7fd6d7d0325bc9b120ec4
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-12-03 09:16:34 +0200

    removed unnecessary field in serializer

--- src/custom_authentication/serializers.py
+++ src/custom_authentication/serializers.py
@@ -4,12 +4,11 @@ from .models import RefreshToken, User
 
 
 class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
+    class Meta:
+        model = User
+
 
 class RefreshTokenSerializer(serializers.ModelSerializer):
-  user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(
-  ), pk_field=serializers.UUIDField(format='hex_verbose'))
-  class Meta:
-    model = RefreshToken
-    fields = ['token', 'expiry_date', 'user']
+    class Meta:
+        model = RefreshToken
+        fields = ['token', 'expiry_date', 'user']
